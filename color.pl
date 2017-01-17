7616090b2c4c9e8c1960e35f8a3213fc1acf3dff
Simone Cesano <simonecesano@libero.it>
2017-01-17 11:20:49 +0100

    added color search and distance

--- color.pl
+++ color.pl
@@ -1,10 +1,46 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
 use Color::Library;
+use Graphics::Color::RGB;
+use List::Util qw/min/;
 
-helper library => sub {
-    return 'Color::Library'
-    };
+sub circle_distance {
+    my ($a, $b, $t) = @_;
+    $t ||= 360;
+    return min((($a - $b) % $t), (($b - $a) % $t));
+};
+
+sub color_distance {
+    my ($a, $b) = @_;
+    return ((
+	    circle_distance($a->h, $b->h) ** 2 +
+	    (($a->s * 100) - ($b->s * 100)) ** 2 +
+	    (($a->v * 100) - ($b->v * 100)) ** 2
+	   ) ** 0.5)
+};
+
+
+helper library => sub { return 'Color::Library' };
+
+helper all_colors => sub {
+    state $all = [ 
+		  map {
+		      my $hsl = Graphics::Color::RGB->from_hex_string($_->{rgb})->to_hsl;
+		      $_->{h} = $hsl->h; $_->{s} = $hsl->s; $_->{l} = $hsl->l;
+		      $_
+		  }
+		  map {
+		      map {
+			  {
+			      rgb => $_->svg,
+				  id => $_->id,
+				  dictionary => $_->dictionary->name,
+				  title => $_->title
+			  }
+		      } $_->colors } Color::Library->dictionaries
+		 ];
+    return $all;
+};
 
 get '/' => sub {
     my $c = shift;
@@ -15,6 +51,20 @@ get '/' => sub {
 		description => $_->description
     } } app->library->dictionaries ]);
 };
+
+get '/all' => sub {
+    my $c = shift;
+    my $all = app->all_colors;
+    $c->render(json => $all);
+};
+
+get '/closest' => sub {
+    my $c = shift;
+    my $all = app->all_colors;
+
+    $c->render(json => $all);
+};
+
 
 get '/:library' => sub {
     my $c = shift;
