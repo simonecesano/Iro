a47588c08b08c273a279609ba84ef75080ae02d8
Simone Cesano <simonecesano@libero.it>
2017-03-04 09:51:51 +0100

    cleanup

--- public/app/events.js
+++ public/app/events.js
@@ -1,11 +1,1 @@
-$(function(){
-    $('#menu_view a').on('click', function(){
-	iro.opts.cameras[iro.activeRendererNumber].offset = offsets[$(this).data('view')];
-    });
-    $('#menu_rotate a').on('click', function(){
-	console.log('here');
-	var axis = $(this).data('rotation')
-	console.log(axis);
-	iro.obj.rotation[axis] = iro.obj.rotation[axis] + 90 * Math.PI / 180
-    });
-})
+
